6e6a186f8b32f147e8e0d51165a2c76d1e9448ef
sean.santhosh.thomas@gmail.com <sean.santhosh.thomas@gmail.com>
2021-01-27 16:40:16 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivymd.uix.tab import MDTabsBase
+from kivy.uix.floatlayout import FloatLayout
+from kivy.metrics import dp
+
+item = """
+
+TwoLineIconListItem:
+	text: "Sj Bros"
+	secondary_text: "Contact"
+	on_press: app.press()
+				
+	ImageLeftWidget:
+		source: "result.png"
+
+"""
+
+class Tab(FloatLayout, MDTabsBase):
+	'''Class implementing content for a tab.'''
+		
+		
+
+class MainApp(MDApp):
+	def build(self):
+		self.theme_cls.primary_palette = "Green"
+		self.title = "Whatsapp"
+		self.root = Builder.load_file("design.kv")
+		
+	
+	def inspect(self, text):
+		print(self.root.ids.main_tab.default_tab)
+		self.title = text
+		
+		
+	def on_start(self):
+		lst = self.root.ids.list
+		for i in range(1,31):
+			list_item = item
+			lst.add_widget(Builder.load_string(list_item))
+	
+	def show_more(self):
+		self.title = "Wait for more :)"
+		
+	def press(self):
+		print("User tried to open a contact but failed.")
+
+
+
+if __name__ == "__main__":
+	MainApp().run()
