42860edca651771c5df6882bd383306e37210e2f
SeanSanthosh <sean.santhosh.thomas@gmail.com>
2021-02-17 21:14:51 +0530

    Added the Main Menu and its functionality.

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.tab import MDTabsBase
 from kivy.uix.floatlayout import FloatLayout
-from kivy.metrics import dp
+from kivymd.uix.menu import MDDropdownMenu
+from kivy.uix.screenmanager import (ScreenManager, Screen)
 
 item = """
 
 TwoLineIconListItem:
 	text: "Sj Bros"
 	secondary_text: "Contact"
-	on_press: app.press(*args)
+	on_press: app.open_contact(*args)
 				
 	ImageLeftWidget:
 		source: "result.png"
@@ -18,7 +19,37 @@ TwoLineIconListItem:
 
 class Tab(FloatLayout, MDTabsBase):
 	'''Class implementing content for a tab.'''
-		
+
+
+class MainScreen(Screen):
+	pass
+
+class SettingsScreen(Screen):
+	pass
+
+
+class NewGroupScreen(Screen):
+	pass
+
+
+class NewBrodcastScreen(Screen):
+	pass
+
+
+class WhatsAppWebScreen(Screen):
+	pass
+
+
+class StarredMessagesScreen(Screen):
+	pass
+
+
+class PaymentsScreen(Screen):
+	pass
+
+
+
+
 		
 
 class MainApp(MDApp):
@@ -27,24 +58,56 @@ class MainApp(MDApp):
 		self.title = "Whatsapp"
 		self.root = Builder.load_file("design.kv")
 		
+		
 	
 	def inspect(self, text):
-		print(self.root.ids.main_tab.default_tab)
 		self.title = text
 		
 		
+
 	def on_start(self):
-		lst = self.root.ids.list
+		lst = self.root.get_screen("main").ids.list
 		for i in range(1,31):
 			list_item = item
 			lst.add_widget(Builder.load_string(list_item))
+
+
+
+	def menu(self, instance):
+		print("Function 'menu' accessed")
+		self.root.ids.settings.manager.current = instance.text.lower().replace(' ', '')
+		print("Changed to screen:",instance.text)	
+	
+	
 	
 	def show_more(self):
-		self.title = "Wait for more :)"
+		menu_items = [
+		{'text': 'New Group'}, 
+		{'text': 'New Brodcast'}, 
+		{'text': 'WhatsApp Web'}, 
+		{'text': 'Starred Messages'}, 
+		{'text': 'Payments'}, 
+		{'text': 'Settings'}
+						]
+		self.main_screen_more_menu = self.create_menu(self.root.get_screen("main").ids.more_menu_caller, menu_items)
+		self.main_screen_more_menu.open()
 		
-	def press(self, obj):
-		print(f"contact list: {obj}")
-		#print("User tried to open a contact but failed.")
+		
+		
+	def open_contact(self, obj):
+		print(obj.text)
+	
+	
+	
+	# Function to create DropDownMenu
+	def create_menu(self, callfrom, menu_items):
+		menu = MDDropdownMenu(caller = callfrom, items = menu_items, width_mult = 4, callback = self.menu)
+		return menu
+		
+	
+	
+	def on_stop(self):
+		raise ValueError("Your Program has worked Successfully!")
 
 
 
